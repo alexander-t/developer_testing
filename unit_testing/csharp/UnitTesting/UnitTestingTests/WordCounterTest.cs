2fffba1fad581bc6f855bb789c7e4aaaaad27554
Alexander <alexander@local>
2013-06-17 00:47:37 +0200

    Added the test project for the unit testing chapter.

---
+++ unit_testing/csharp/UnitTesting/UnitTestingTests/WordCounterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestingTests
+{
+    [TestClass]
+    public class WordCounterTest
+    {
+        [TestMethod]
+        public void TwoIdenticalAndTwoDifferentWords_ThreeDistinctWords()
+        {
+            // Arrange
+            var wc  = new WordCounter();
+            
+            // Act
+            wc.Add(new List<string> { "CCC", "AAA", "BBB", "AAA" });
+            var countedWords = wc.WordCountPairs.Count;
+            
+            // Assert
+            Assert.AreEqual(3, countedWords);
+        }
+    }
+}
