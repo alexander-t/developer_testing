42d7f15aa811f3e13a984041033ee3e79b88eb68
Alexander <alexander@local>
2013-11-21 23:10:31 +0100

    Added theory test in c#

---
+++ parameterized_tests/csharp/ParameterizedTests/Domain/PremiumRuleEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum Gender { MALE, FEMALE, UNKNOWN}
+    
+    public class PremiumRuleEngine 
+    {
+        public double GetPremiumFactor(int age, Gender gender)
+        {
+            double genderMultiplier = gender
+                    == Gender.FEMALE ? 0.9 : 1;
+            if (age >= 18 && age < 24)
+            {
+                return 1.75 * genderMultiplier;
+            }
+            else if (age >= 24 && age < 60)
+            {
+                return 1 * genderMultiplier;
+            }
+            else if (age >= 60)
+            {
+                return 1.35 * genderMultiplier;
+            }
+            throw new ArgumentOutOfRangeException(
+                    "Age out of range");
+        }
+    }
+    
+}
---
+++ parameterized_tests/csharp/ParameterizedTests/DomainTests/PremiumFactorsWithinRangeTest.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DomainTests
+{
+    public class PremiumFactorsWithinRangeTest
+    {
+        [Datapoints]
+        public Gender[] genders
+                = new Gender[]{Gender.FEMALE,
+                Gender.MALE, Gender.UNKNOWN};
+
+        [Datapoints]
+        public int[] ages
+                = new int[]{17, 18, 19, 23, 24, 25,
+            59, 60, 61, 100, 101};
+
+        [Theory]
+        public void PremiumsFactorsAreBetween0_5and2_0(Gender gender, int age)
+        {
+            Assume.That(age, Is.GreaterThanOrEqualTo(18));
+            Assume.That(age, Is.LessThanOrEqualTo(100));
+            Assume.That(gender == Gender.FEMALE || gender == Gender.MALE);
+            double premiumFactor
+                    = new PremiumRuleEngine().GetPremiumFactor(age, gender);
+            Assert.That(premiumFactor, Is.InRange(0.5, 2.0));
+        }
+    }
+}
